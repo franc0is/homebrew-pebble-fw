ec7a6ebda673cc5abb752234c3d6768bec1572ac
Liam McLoughlin <hexxeh@hexxeh.net>
2015-09-23 23:50:02 +0100

    Add Bottle URLs

--- arm-none-eabi-gcc.rb
+++ arm-none-eabi-gcc.rb
@@ -4,6 +4,11 @@ class ArmNoneEabiGcc < Formula
   url "https://launchpad.net/gcc-arm-embedded/4.8/4.8-2014-q2-update/+download/gcc-arm-none-eabi-4_8-2014q2-20140609-mac.tar.bz2"
   sha256 "dbfa2170e67f30aad349581b7db8ce61f257c3a51e32943ec12a384b1dcd7cf9"
 
+  bottle do
+    root_url "http://pebble-homebrew.s3.amazonaws.com"
+    sha256 "02ed7a6e8aa02e5794c8e822ac0bc64bd7497a78e4237ba5db3f7102e376b967" => :yosemite
+  end
+
   resource "sources" do
     url "https://launchpad.net/gcc-arm-embedded/4.8/4.8-2014-q2-update/+download/gcc-arm-none-eabi-4_8-2014q2-20140609-src.tar.bz2"
     sha256 "5c0843c5e4897ab3379523c2094208c0a7192a74d5bf25a9e545295696563e5a"
