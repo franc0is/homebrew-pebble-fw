8570612c32c08d73fcab771605ae2115a91efd22
Katharine Berry <katharine@getpebble.com>
2015-10-20 13:38:03 -0700

    Rename pebble-qemu to pebble-qemu-dev.

--- pebble-qemu-dev.rb
+++ pebble-qemu-dev.rb
@@ -1,4 +1,4 @@
-class PebbleQemu < Formula
+class PebbleQemuDev < Formula
   homepage "https://github.com/pebble/qemu"
   url "git@github.com:pebble/qemu-dev.git", :revision => "74ee1bd", :using => :git
   head "git@github.com:pebble/qemu-dev.git", :using => :git
